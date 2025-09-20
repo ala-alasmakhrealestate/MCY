4c1441cf691c285cda736fb02d78bcaf1c5260b9
AlaEddine Ben Ali <alaa@admins-MacBook-Pro.local>
2025-09-20 13:16:39 +0300

    fixing 404 error

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,9 @@
-// import type { NextConfig } from "next";
-//
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-//
-// export default nextConfig;
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     reactStrictMode: true,
     eslint: {
         ignoreDuringBuilds: true,
     },
-    content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-    },
-    plugins: []
 }
+
+module.exports = nextConfig
