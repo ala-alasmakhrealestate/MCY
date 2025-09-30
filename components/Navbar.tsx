8b899355da8480e513b547e63c0614406fd25f76
AlaEddine Ben Ali <alaa@admins-MacBook-Pro.local>
2025-09-30 10:44:30 +0300

    remove fr option

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = ({ locale }: { locale: string }) => {
                 className="rounded-md bg-white px-4 py-2 bg-transparent border border-gray-300 hover:outline-none focus:outline-none"
             >
                 <option value="en">EN</option>
-                <option value="fr">FR</option>
+                {/*<option value="fr">FR</option>*/}
                 <option value="ar">AR</option>
             </select>
         </div>
